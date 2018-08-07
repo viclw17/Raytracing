3811d9e401ce742bfdda050f1b4688f864149e19
Victor Li <viclw17@gmail.com>
2018-08-07 01:21:04 -0700

    Xcode main.cpp debug.

--- RaytracingInOneWeekend/xcode/raytracer/raytracer/main.cpp
+++ RaytracingInOneWeekend/xcode/raytracer/raytracer/main.cpp
@@ -61,27 +61,49 @@ vec3 color(const ray& r, hitable *world, int depth) {
 int main() {
     int nx = 200;
     int ny = 100;
-//    nx = 800;
-//    ny = 400;
+    nx = 800;
+    ny = 400;
     int ns = 100;
 
     ofstream outfile("test.ppm", ios_base::out);
     outfile << "P3\n" << nx << " " << ny << "\n255\n";
     //std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    hitable *list[4]; // 一个储存有4个“指向hitable对象的指针”的数组
+    int sphere_num = 4;
+    hitable *list[sphere_num]; // 一个储存有4个“指向hitable对象的指针”的数组
+    
+    float big_r = 5000.0;
+    float z = -1.0;
+    
+    // metal
 //    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
 //    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
 //    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.5));
 //    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8,0.8,0.8), 0.0));
     
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(1,1,1)));
+    list[1] = new sphere(vec3(-1.1,0,-1.5), 0.5, new metal(vec3(0.8,0.3,0.3), 0.3));
+    list[2] = new sphere(vec3(0,0,-1.4), 0.5, new metal(vec3(0.8,0.8,0.8), 0.0));
+    list[3] = new sphere(vec3(1.1,0,-1.5), 0.5, new lambertian(vec3(0.8,0.6,0.2)));
+    
+    // dielectric
+//    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+//    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+//    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
+//    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    
+    
+//    list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(1,1,1)));
+//    list[1] = new sphere(vec3(0,0,z), 0.5, new dielectric(1.5));
+//    list[2] = new sphere(vec3(-.7,0,z-.5), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+//    list[3] = new sphere(vec3(.7,0,z-.5), 0.5, new lambertian(vec3(0.8,0.6,0.2)));
+
+
+    
+
 
     // world是一个指向hitable对象的指针变量
-    hitable *world = new hitable_list(list, 4);
+    hitable *world = new hitable_list(list, sphere_num);
     camera cam;
     for(int j=ny-1; j>=0; j--) {
         for(int i=0; i<nx; i++) {
