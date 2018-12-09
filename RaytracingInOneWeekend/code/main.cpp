5ed93d840dfe588ea4b05b0b4e231fc07a3d11b0
viclw17 <viclw17@gmail.com>
2018-12-08 20:13:29 -0800

    Add preprocessor for multiple test scenes/cameras.

--- RaytracingInOneWeekend/code/main.cpp
+++ RaytracingInOneWeekend/code/main.cpp
@@ -9,6 +9,21 @@
 #include "dielectric.h"
 using namespace std;
 
+#define TESTSCENE 7
+//1 // diffuse, blogpost scene
+//2 // metal, book scene
+//3 // metal, blogpost scene
+//4 // dielectric, book scene
+//5 // dielectric, white background, 3 colors, 3 materials, pyramid
+//6 // dielectric, white background, 3 colors, 3 materials
+//7 // dielectric, blogpost scene
+//0 // cover image scene
+#define TESTCAM 2
+//1 // Camera angled
+//2 // Camera facing forward	
+//0 // Camera cover image
+
+
 // r: reference to a Ray object
 // world: pointer to a Hitable object
 vec3 color(const ray& r, hitable *world, int depth) {
@@ -32,30 +47,6 @@ vec3 color(const ray& r, hitable *world, int depth) {
         return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0); // lerp
     }
 }
-
-////////////////////////////////////////////////////////////
-// old test
-// bool hit_sphere(const vec3& center, float radius, const ray& r) {
-//     vec3 oc = r.origin() - center;
-//     float a = dot(r.direction(), r.direction());
-//     float b = 2.0 * dot(oc, r.direction());
-//     float c = dot(oc,oc) - radius*radius;
-//     float discriminant = b*b - 4*a*c;
-//     return (discriminant>0);
-// }
-//
-// vec3 color(const ray& r) {
-//     // draw blue sphere
-//     if(hit_sphere(vec3(0,0,-1), 0.5, r))
-//         return vec3(0,0,1);
-//
-//     // draw background
-//     // vec3 unit_direction = unit_vector(r.direction());
-//     // float t = 0.5*(unit_direction.y()+1.0); // -1~1 --> 0~1
-//     // return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0); // lerp
-//     return vec3(1.0,1.0,1.0);
-// }
-////////////////////////////////////////////////////////////
 
 // Cover image
 hitable *random_scene() {
@@ -115,103 +106,92 @@ hitable *random_scene() {
     }
 
 ////////////////////////////////////////////////////////////
-
 int main() {
-    int nx = 100;
-    int ny = 50;
-    nx = 200;
-    ny = 100;
-    //nx = 800;
-    //ny = 400;
-	nx = 1000;
-	ny = 500;
-    int ns = 200;
+	// resolution
+	int nx = 100;
+	int ny = 50;
+	//nx = 200;
+	//ny = 100;
+	//nx = 800;
+	//ny = 400;
+	//nx = 1000;
+	//ny = 500;
 
-    ofstream outfile("test.ppm", ios_base::out);
-    outfile << "P3\n" << nx << " " << ny << "\n255\n";
-    //std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	// msaa
+	int ns = 100;
+	//ns = 1;
 
-    
+    // create scene
     int sphere_num = 4;
     hitable *list[4]; // 一个储存有4个“指向hitable对象的指针”的数组
-    float big_r = 5000.0;
-    float z = -1.0;
-    
+    float big_r = 5000.0; // ground sphere
+    float z = -1.0; // sphere z position
 
-    // diffuse, blog arrangement
-    /*
+	// create world
+	hitable *world;
+	world = new hitable_list(list, sphere_num);
+    
+	////////////////////////////////////////////////////////////
+	// scene defination
+	#if TESTSCENE == 1 // diffuse, blogpost scene
     list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(.5,.5,.5)));
     list[1] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new lambertian(vec3(.7,.7,.7)));
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new lambertian(vec3(1,1,1)));
-    */
 	
-    // metal, book default arrangement
-    /*
+	#elif TESTSCENE == 2 // metal, book scene
     list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.5));
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8,0.8,0.8), 0.0));
-    */
-    // metal, blog arrangement
-    /*
+    
+	#elif TESTSCENE == 3 // metal, blogpost scene
     list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(.5,.5,.5)));
     list[1] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.5));
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8,0.8,0.8), 0.0));
-    */
 
-    // dielectric, book default arrangement
-    /*
+	#elif TESTSCENE == 4 // dielectric, book scene
     list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.0));
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(vec3(1, 1, 1),1.5));
-    */
-
-    // white background, 3 colors, 3 materials, pyramid
-    /*
+   
+	#elif TESTSCENE == 5 // dielectric, white background, 3 colors, 3 materials, pyramid
     list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(1,1,1)));
     list[1] = new sphere(vec3(0,0,z), 0.5, new dielectric(vec3(0.8,0.7,0.5), 1.5));
     list[2] = new sphere(vec3(-.7,0,z-0.5), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
     list[3] = new sphere(vec3( .7,0,z-0.5), 0.5, new metal(vec3(0.8,0.2,0.2), 0.5));
-    */
 
-    // white background, 3 colors, 3 materials
-    /*
+	#elif TESTSCENE == 6 // dielectric, white background, 3 colors, 3 materials
     list[0] = new sphere(vec3(0,-(big_r+0.5),z), big_r, new lambertian(vec3(1,1,1)));
     list[1] = new sphere(vec3(-1,0,z), 0.5, new dielectric(vec3(0.9,0.8,0.5), 1.5));
     list[2] = new sphere(vec3(0,0,z), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
     list[3] = new sphere(vec3( 1,0,z), 0.5, new metal(vec3(0.8,0.3,0.3), 0.5));
-    */
 
-    // blue background, white color, 3 materials
-    
+	#elif TESTSCENE == 7 // dielectric, blogpost scene
     list[0] = new sphere(vec3(0, -(big_r + 0.5), z), big_r, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, 0, z), 0.5, new dielectric(vec3(.9,.9,.9), 1.5));
     list[2] = new sphere(vec3(-1.001, 0, z), 0.5, new lambertian(vec3(.8,.8,.8)));
     list[3] = new sphere(vec3(1, 0, z), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3)); //0.5
     //list[4] = new sphere(vec3(0, 0, z), -0.45, new dielectric(vec3(1, 1, 1), 1.5));r
-    
 
-    // world是一个指向hitable对象的指针变量
-    hitable *world;
-    world = new hitable_list(list, sphere_num);
-    //world = random_scene(); // cover image
+	#elif TESTSCENE == 0 // cover image scene
+	world = random_scene();
+	
+	#endif //TESTSCENE
 
-    ////////////////////////////////////////////////////////////
-    // Camera angled
-    /*
+	////////////////////////////////////////////////////////////
+    // camera defination
+	#if TESTCAM == 1 // Camera angled
     vec3 lookfrom(-2, 1.5, 1);//(3, 3, 2);
     vec3 lookat(-0.2, 0, -1);
     float dist_to_focus = (lookfrom - lookat).length();
     float aperture = .01; // 0.5
     float theta = 40;//20
-    //camera cam(lookfrom, lookat, vec3(0, 1, 0), theta, float(nx) / float(ny), aperture, dist_to_focus);
-    */
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), theta, float(nx) / float(ny), aperture, dist_to_focus);
 
-    // Camera facing forward
-	
+	#elif TESTCAM == 2 // Camera facing forward	
     vec3 lookfrom = vec3(0,1,1.5);
     vec3 lookat = vec3(0, 0, -1);
     float dist_to_focus = (lookfrom - lookat).length();
@@ -219,17 +199,19 @@ int main() {
     float theta = 40;//20
     camera cam(lookfrom, lookat, vec3(0, 1, 0), theta, float(nx) / float(ny), aperture, dist_to_focus);
 	
-
-    // Camera, cover image
-    /*
+	#elif TESTCAM == 0 // Camera cover image
     vec3 lookfrom(11,2,3);
     vec3 lookat(0,0.6,0);
     float dist_to_focus = (lookfrom - lookat).length();
     float aperture = 0.0;
     camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture,0.7*dist_to_focus);
-    */
-    ////////////////////////////////////////////////////////////
+	
+	#endif //TESTCAM
 
+    ////////////////////////////////////////////////////////////
+	// image output
+	ofstream outfile("test.ppm", ios_base::out);
+	outfile << "P3\n" << nx << " " << ny << "\n255\n";
     int total = nx*ny;
     int current = 0;
     int counter = 0;
@@ -245,12 +227,6 @@ int main() {
                 // float u = float(i) / float(nx);
                 // float v = float(j) / float(ny);
                 ray r = cam.get_ray(u,v); // generate ray per sample
-
-                // old test
-                // vec3 p = r.point_at_parameter(1.0);
-                // p = unit_vector(p);
-                // col = vec3(p.z(),p.z(),p.z());
-                // col += color(r);
 
                 col += color(r, world, 0);
             }
@@ -271,8 +247,7 @@ int main() {
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
 
-            outfile   << ir << " " << ig << " " << ib << "\n";
-            //std::cout << ir << " " << ig << " " << ib << "\n";
+            outfile << ir << " " << ig << " " << ib << "\n";
         }
     }
     cout << endl;
